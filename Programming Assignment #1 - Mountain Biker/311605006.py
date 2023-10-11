cf9f672a77098fd8959c610b3a43e71513bdf3d7
AMNshaw <rosel94638@gmail.com>
2023-10-11 11:27:29 +0800

    revised

--- Programming Assignment #1 - Mountain Biker/311605006.py
+++ Programming Assignment #1 - Mountain Biker/311605006.py
@@ -8,16 +8,6 @@ def closest(points):
 def computeDistance(pt1, pt2):
     return math.sqrt((pt1[0] - pt2[0]) ** 2 + (pt1[1] - pt2[1]) ** 2)
 
-def stripClosest(s, delta):
-    stripSize = len(s)
-    for i in range(stripSize):
-            for j in range(i+1, min(i+7, stripSize)):
-                if(abs(s[i][1] - s[j][1]) < delta):
-                    d = computeDistance(s[i], s[j])
-                    if(d < delta):
-                        delta = d
-    return delta
-
 def recursion(Px, Py):
     n = len(Px)
     if(n <= 3):
@@ -29,11 +19,15 @@ def recursion(Px, Py):
         Ly = []
         Ry = []
         for pt in Py:
-            Ly.append(pt) if (pt[0] <= midPt[0]) else Ry.append(pt)
+            if (pt[0] <= midPt[0]):
+                Ly.append(pt)
+            else:
+                Ry.append(pt)
+
         
         delta_L = recursion(Lx, Ly)
         delta_R = recursion(Rx, Ry)
-        delta = delta_L if (delta_L < delta_R) else delta_R
+        delta = min(delta_L, delta_R)
         s = []
         for pt in Py:
             if(abs(pt[0] - midPt[0]) < delta):
@@ -42,6 +36,16 @@ def recursion(Px, Py):
         delta = stripClosest(s, delta)
 
         return delta
+
+def stripClosest(s, delta):
+    stripSize = len(s)
+    for i in range(stripSize):
+            for j in range(i+1, min(i+7, stripSize)):
+                if(abs(s[i][1] - s[j][1]) < delta):
+                    d = computeDistance(s[i], s[j])
+                    if(d < delta):
+                        delta = d
+    return delta
 
 def bruteForce(P):
     n = len(P)
